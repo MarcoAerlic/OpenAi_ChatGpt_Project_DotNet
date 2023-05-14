1d0fcb02dcaccf2eb4fa9acdc0fae44808527414
MarcoAerlic <100177448+MarcoAerlic@users.noreply.github.com>
2023-05-14 23:01:09 +1000

    Add files via upload

---
+++ MarcosWebApiProject/Controllers/ADGeneratorController.cs
@@ -0,0 +1,37 @@
+﻿using MarcosWebApiProject.ApplicationService.ADProduct;
+using MarcosWebApiProject.ApplicationService.Interfaces;
+using Microsoft.AspNetCore.Mvc;
+
+namespace MarcosWebApiProject.API.Controllers
+{
+    [Route("api/adgenerator/[action]")]
+    [ApiController]
+    public class ADGeneratorController : ControllerBase
+    {
+
+        private readonly IADProductService _adProduct;
+        public ADGeneratorController(IADProductService adProduct)
+        {
+            _adProduct = adProduct;
+        }
+
+        [HttpPost]
+        public async Task<ActionResult<ADProductResponseModel>> GenerateAD(CustomerRequestModel aDGenerateRequestModel)
+        {
+            try
+            {
+
+                var response = await _adProduct.GenerateAdContent(aDGenerateRequestModel);
+
+                return response;
+            }
+            catch (System.Exception ex)
+            {
+
+                return null;
+            }
+
+        }
+    }
+
+}
---
+++ MarcosWebApiProject/Program.cs
@@ -0,0 +1,31 @@
+using MarcosWebApiProject.ApplicationService.ADProduct;
+using MarcosWebApiProject.ApplicationService.Interfaces;
+using MarcosWebApiProject.Infrastructure.Network;
+
+var builder = WebApplication.CreateBuilder(args);
+
+// Add services to the container.
+
+builder.Services.AddControllers();
+// Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddSwaggerGen();
+builder.Services.AddScoped<IADProductService, ADProductService>();
+builder.Services.AddScoped<IBotAPIService, BotAPIService>();
+
+var app = builder.Build();
+
+// Configure the HTTP request pipeline.
+if (app.Environment.IsDevelopment())
+{
+    app.UseSwagger();
+    app.UseSwaggerUI();
+}
+
+app.UseHttpsRedirection();
+
+app.UseAuthorization();
+
+app.MapControllers();
+
+app.Run();
